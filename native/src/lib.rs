211e55d0d5829187922ac09c26e5843d3694a276
reinkepatrick <patrick.reinke@fh-bielefeld.de>
2018-05-23 17:40:32 +0200

    add socket.io interface

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -4,11 +4,11 @@ extern crate neon;
 use neon::vm::{Call, JsResult};
 use neon::js::JsString;
 
-fn hello(call: Call) -> JsResult<JsString> {
+fn get_move(call: Call) -> JsResult<JsString> {
     let scope = call.scope;
-    Ok(JsString::new(scope, "hello node").unwrap())
+    Ok(JsString::new(scope, "e2e4").unwrap())
 }
 
 register_module!(m, {
-    m.export("hello", hello)
+    m.export("getMove", get_move)
 });
