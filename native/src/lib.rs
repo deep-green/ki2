5c60bad8890cc1f0e3fabeb20e33693e9033e85c
reinkepatrick <patrick.reinke@fh-bielefeld.de>
2018-06-15 17:02:58 +0200

    add move generator

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,11 +1,10 @@
 #[macro_use]
 extern crate neon;
-extern crate chess;
 
 use neon::vm::{Call, JsResult, This, FunctionCall};
 use neon::js::{JsString, Value};
 
-use chess::{ MoveGen, Board };
+mod mov;
 
 
 trait CheckArgument<'a> {
@@ -21,26 +20,7 @@ impl<'a, T: This> CheckArgument<'a> for FunctionCall<'a, T> {
 fn get_move(mut call: Call) -> JsResult<JsString> {
     let fen: String = call.check_argument::<JsString>(0)?.value();
 
-    let board = Board::from_fen(fen.to_owned()).unwrap();
-    let iterable = MoveGen::new(board, true);
-
-    let mut ret: String = "[".to_string();
-    let mut count: usize = 0;
-    let size: usize = iterable.len();
-
-    for item in iterable {
-        ret.push_str(&item.to_string());
-
-        if count < size - 1 {
-            ret.push_str(&", ");
-        }
-
-        count += 1;
-    }
-
-    ret.push_str(&"]");
-
-    Ok(JsString::new(call.scope, &ret).unwrap())
+    Ok(JsString::new(call.scope, &mov::get_moves(fen)).unwrap())
 }
 
 register_module!(m, {
